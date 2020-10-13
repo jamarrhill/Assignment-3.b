e8d533df43d71a824ebe65eecf933492550794e0
jamarrhill <hilljama@oregonstate.edu>
2020-10-13 10:45:55 -0800

    Assignment 3.b

---
+++ factors.py
@@ -0,0 +1,36 @@
+# Name: Jamar Hill
+# Date: 10/13/2020
+# Description: Assignment 3.b
+
+print("Please enter a positive integer:")
+var = int(input())
+print("The factors of", var, "are:")
+for num in range(1, var+1, 1):
+    if(var % num == 0):
+        print(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
